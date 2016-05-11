35806ac17dbf27aed604d0cf5e5ec1d04e0b3a82
César Magalhães <ceanma@gmail.com>
2016-05-11 17:54:08 -0300

    Foi criado o arquivo FreedomGMS.sql

--- docs/database/FreedomCFG.sql
+++ docs/database/FreedomCFG.sql
@@ -5,3 +5,5 @@ select idgrpusu,nomegrpusu,comentgrpusu,dtins,hins,idusuins,dtalt,halt,idusualt 
 --insert into sggrpusu (codemp,codfilial,idgrpusu,nomegrpusu,comentgrpusu) values (?,?,?,?,?)
 --delete from sggrpusu where codemp=? and codfilial = ? and idgrpusu=?
 --update sggrpusu set  nomegrpusu=?,comentgrpusu=? where idgrpusu=? and codemp=? and codfilial=?
+SELECT idgrpusu,nomegrpusu  FROM sggrpusu;
+SELECT codcc,anocc,desccc  FROM fncc WHERE CODEMP=99 AND CODFILIAL=1 AND NIVELCC=10 AND ANOCC=2013
--- docs/database/FreedomCRM.sql
+++ docs/database/FreedomCRM.sql
@@ -3,7 +3,20 @@ SELECT *  FROM sgcnae where codcnae like '62.01-5%';
 SELECT *  FROM sgcnae where codcnae like '96.02-5%';
 SELECT codcnae,desccnae  FROM sgcnae where desccnae like '%programa%';
 SELECT codcnae,desccnae  FROM sgcnae where desccnae like '%beleza%';
+SELECT codcnae,desccnae  FROM sgcnae where desccnae like '%papelaria%';
+SELECT codcnae,desccnae  FROM sgcnae where desccnae like '%civil%';
+SELECT codcnae,desccnae  FROM sgcnae where desccnae like '%transporte%';
+
 
 --Busca código do banco
 SELECT *  FROM fnbanco where nomebanco like '%Santander%';
 SELECT codbanco,nomebanco  FROM fnbanco where nomebanco like '%Santander%';
+
+SELECT codsetor,descsetor  FROM vdsetor WHERE CODEMP=99 AND CODFILIAL=1;
+
+SELECT codclascli,descclascli  FROM vdclascli WHERE CODEMP=99 AND CODFILIAL=1;
+SELECT codtipocli,desctipocli  FROM vdtipocli WHERE CODEMP=99 AND CODFILIAL=1;
+SELECT codmarca,descmarca,siglamarca,dtins,hins,idusuins,dtalt,halt,idusualt  FROM eqmarca WHERE CODEMP=99 AND CODFILIAL=1;
+SELECT codfisc,descfisc  FROM lfclfiscal;
+SELECT *  FROM lfclfiscal;
+SELECT codvarg,descvarg  FROM eqvargrade WHERE CODEMP=99 AND CODFILIAL=1;
